c5b542723308279c96286e3354ba216968330b33
Hache <florian.hachenberger@gmail.com>
2017-03-22 13:12:01 +0100

    Bank v2

---
+++ src/com/dhbw/dhbw2017/programming/bank/v2/Account.java
@@ -0,0 +1,77 @@
+package com.dhbw.dhbw2017.programming.bank.v2;
+
+/**
+ * Created by flori on 22.03.2017.
+ */
+public class Account {
+
+    private static Account[] accounts = new Account[1000];
+    private static int lastAccountNumber = 0;
+
+    private int accountNumber;
+    private double balance;
+
+    public Account(){
+        balance= 0.00;
+        accountNumber=++lastAccountNumber;
+        accounts[accountNumber]=this;
+        System.out.println("Number of the created account is: "+ lastAccountNumber);
+    }
+
+
+    public void deposit( double amount){
+        balance += amount;
+        System.out.println("Succesfully added "+amount+"EUR to the account "+accountNumber);
+    }
+
+    public void printBalance(){
+        System.out.println("The balance of account "+accountNumber+" is "+checkBalance()+" €"); ;
+    }
+    public double checkBalance(){
+        return balance;
+    }
+
+    public  void withdraw(double amount){
+
+        if(checkBalance()-amount>0){
+            balance -= amount;
+            System.out.println("Successfully withdrawn "+amount+"€ from the account "+accountNumber);
+        }else{
+            System.out.println("Unsufficient funds");
+        }
+
+    }
+
+
+    public void transfer( Account receivingAccount, double amount){
+        if(checkBalance()-amount>0){
+            withdraw(amount);
+            receivingAccount.deposit(amount);
+        }else{
+            System.out.println("Unsufficient funds");
+        }
+    }
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public static Account[] getAccounts() {
+        return accounts;
+    }
+
+    public static void setAccounts(Account[] accounts) {
+        Account.accounts = accounts;
+    }
+}
--- src/com/dhbw/dhbw2017/programming/bank/v2/Bank.java
+++ src/com/dhbw/dhbw2017/programming/bank/v2/Bank.java
@@ -15,6 +15,52 @@ public class Bank {
         this.name = name;
     }
 
+
+    public static void main(String[] args) {
+        // create 100 initial accounts
+
+        // new customer
+        // old implementation int accountNumberCustomer1 = createAccount();
+        Account accountCustomer1 = new Account();
+        // customer wants to deposit 50 EUR
+        // old implementation: deposit(accountNumberCustomer1, 50.00);
+        accountCustomer1.deposit(50.00);
+        // customer wants to know the balance of his account.
+        // old implementation: printBalance(accountNumberCustomer1);
+        accountCustomer1.printBalance();
+        // the customer wants to withdraw 30 EUR from his account
+       // old implementation:  withdraw(accountNumberCustomer1,30.00);
+       accountCustomer1.withdraw(30);
+        // the customer wants to transfer 25 EUR to the account with the number v2
+        // old implementation = transfer(accountNumberCustomer1,2,25);
+        Account receivingAccount = new Account();
+        accountCustomer1.transfer(receivingAccount,25);
+
+        // the bank employee wants to add the interest for all accounts at the end of the year
+        // print the balance of all accounts before the interest calculation
+        // The interest rate is 2.5%
+        // print the balance of all accounts after the interest calculation
+        calculateInterest(2.5f);
+    }
+
+    public static void calculateInterest(float interestRate){
+        Account[] accounts = Account.getAccounts();
+        for (Account a:accounts){
+            if(a!=null) {
+                System.out.println("Before interest calculation");
+                a.printBalance();
+                a.deposit(a.checkBalance() * (interestRate / 100));
+                System.out.println("After interest calculation");
+                a.printBalance();
+            }
+        }
+    }
+
+
+
+
+
+
     public String getName() {
         return name;
     }
